d3e14206c30239e7897d41619a460aef4142a923
LowLevelLemmy <77054159+LowLevelLemmy@users.noreply.github.com>
2021-08-31 11:46:31 -0700

    Made zombies jump out of barricades 💦💦💦😅😅😅😓😓😓 It was the hardest thing ever, and it looks AWFUL

--- Blind/Assets/Folders/Scripts/Zombie/ZombieSpawner.cs
+++ Blind/Assets/Folders/Scripts/Zombie/ZombieSpawner.cs
@@ -15,18 +15,22 @@ public class ZombieSpawner : MonoBehaviour
     public UnityEvent<GameObject> OnZombieSpawned;
     public UnityEvent<GameObject> OnZombieRemoved;
 
+    [SerializeField] float jumpPower;
+    [SerializeField] float dur;
+
+
     [Button]
     public void SpawnZombie(int i = -999)
     {
         if (i == -999)  // do random value if I is null
             i = Random.Range(0, spawnLocations.Count);
 
-        var spawnedZom = Instantiate(zombiePrefab, spawnLocations[i].position, spawnLocations[i].rotation);
+        GameObject spawnedZom = Instantiate(zombiePrefab, spawnLocations[i].position, spawnLocations[i].rotation);
         spawnedZombies.Add(spawnedZom);
-        //spawnedZom.GetComponent<ZombieController>().OnDeath.AddListener(OnZombieKilled);
+        spawnedZom.GetComponent<ZombieController>().OnDeath.AddListener(OnZombieKilled);
         spawnedZom.GetComponent<Animator>().SetTrigger("RunJump");
 
-
+        AnimateJump(spawnedZom.transform, spawnLocations[i].GetChild(0).position);
         OnZombieSpawned?.Invoke(spawnedZom);
     }
 
@@ -35,5 +39,10 @@ public class ZombieSpawner : MonoBehaviour
         spawnedZombies.Remove(zomOb);   // remove zombie from list
         OnZombieRemoved?.Invoke(zomOb);
         zomOb.GetComponent<ZombieController>().OnDeath.RemoveListener(OnZombieKilled);
+    }
+
+    void AnimateJump(Transform tran, Vector3 landingPos)
+    {
+        tran.DOJump(landingPos, jumpPower, 0, dur);
     }
 }
