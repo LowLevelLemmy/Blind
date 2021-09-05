524870ac8418882883a14cc4d16179c5ba1f634f
LowLevelLemmy <77054159+LowLevelLemmy@users.noreply.github.com>
2021-09-05 14:07:40 -0700

    Made doors effect the navmesh

--- Blind/Assets/Folders/Scripts/Door.cs
+++ Blind/Assets/Folders/Scripts/Door.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using EasyButtons;
 
 public class Door : MonoBehaviour, IInteractable
 {
     [SerializeField] int pointsToUnlock;
     [SerializeField] int roomIndexToUnlock;
+    bool unlocked = false;
 
     public string interactTxt => "Press F to Open Door [Cost: " + pointsToUnlock + "]";
     public float maxDist => 3;
@@ -15,6 +17,10 @@ public class Door : MonoBehaviour, IInteractable
 
     public void OnInteractedWith(PlayerInteractor plrInteractor)
     {
+        if (unlocked)
+            return;
+        unlocked = true;
+
         PointsMan pMan = GameObject.FindObjectOfType<PointsMan>();
         if (pMan.playerPoints < pointsToUnlock)
             return;
@@ -25,6 +31,12 @@ public class Door : MonoBehaviour, IInteractable
         if (!dir.unlockedRoomIndexs.Contains(roomIndexToUnlock))    //unlock room so zombies can spawn in it
             dir.unlockedRoomIndexs.Add(roomIndexToUnlock);
 
+        UnlockDoor();
+    }
+
+    [Button]
+    void UnlockDoor()
+    {
         transform.DOLocalMoveY(5, 2).OnComplete(() => Destroy(gameObject));
     }
 
