6d212926d5834eb329dc200aec20c79dc726fbc5
LowLevelLemmy <77054159+LowLevelLemmy@users.noreply.github.com>
2021-09-05 21:45:37 -0700

    Made Carbine penetrative LETS GO

--- Blind/Assets/Folders/Scripts/Weapons/Carbine.cs
+++ Blind/Assets/Folders/Scripts/Weapons/Carbine.cs
@@ -6,6 +6,7 @@ public class Carbine : AbstractWeapon
 {
     [SerializeField] Transform muzzle;
     [SerializeField] GameObject particles;
+    [SerializeField] int penetrationCount = 2;
 
     public override void Fire()
     {
@@ -19,10 +20,27 @@ public class Carbine : AbstractWeapon
 
         RaycastHit hit;
         Vector3 bulletDirection = playerCam.forward;
-        if (Physics.Raycast(playerCam.position, bulletDirection, out hit, 1000, layerMask, QueryTriggerInteraction.Ignore))
+
+        var cols = Physics.RaycastAll(playerCam.position, bulletDirection, 1000, layerMask, QueryTriggerInteraction.Ignore);
+
+        int penCount = penetrationCount;
+        List<IHurtable> enemiesHit = new List<IHurtable>();
+        foreach(var col in cols)
         {
-            if (hit.transform.root.TryGetComponent<IHurtable>(out var hurtable))
-                hurtable.OnHurt();
+            if (col.transform.root.TryGetComponent<IHurtable>(out var hurtable))
+            {
+                if (!enemiesHit.Contains(hurtable))
+                    enemiesHit.Add(hurtable);
+            }
+        }
+
+        foreach(var hurt in enemiesHit)
+        {
+            if (penCount <= 0)
+                break;
+
+            hurt.OnHurt();
+            --penCount;
         }
         base.Fire();
 
