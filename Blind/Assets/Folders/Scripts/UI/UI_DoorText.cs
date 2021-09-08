437df9fdbff3e8c86a5e3aa72fe897b03c37f0a1
LowLevelLemmy <77054159+LowLevelLemmy@users.noreply.github.com>
2021-09-07 20:54:11 -0700

    Made UI even BETTERER

--- Blind/Assets/Folders/Scripts/UI/UI_DoorText.cs
+++ Blind/Assets/Folders/Scripts/UI/UI_DoorText.cs
@@ -6,21 +6,27 @@ using TMPro;
 public class UI_DoorText : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI doorTxt;
+    [SerializeField] GameObject bg;
 
     void OnEnable()
     {
         PlayerInteractor plrInt = GameObject.FindObjectOfType<PlayerInteractor>();
         plrInt.OnInteractTxtShouldComeUpNow += OnLookedAtTxt;
         plrInt.OnLostSightOfInteractable += OnLostSight;
+
+        doorTxt.text = "";
+        bg.SetActive(false);
     }
 
     void OnLookedAtTxt(IInteractable interactable)
     {
+        bg.SetActive(true);
         doorTxt.text = interactable.interactTxt;
     }
 
     void OnLostSight(IInteractable interactable)
     {
+        bg.SetActive(false);
         doorTxt.text = "";
     }
 }
